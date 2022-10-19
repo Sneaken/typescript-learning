568451c8fb4f984d21c59581c948cf1e0eb973e2
Sneaken <924393527@qq.com>
2022-10-19 21:15:11 +0800

    docs: 工具类型

---
+++ notes/utility-types.ts
@@ -0,0 +1,28 @@
+// 1. ReturnType<Type>
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never;
+
+// 2. Parameters<Type>
+type MyParameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
+// 3. Exclude<UnionType, ExcludedMembers>
+type MyExclude<T, U> = T extends U ? never : T;
+
+// 4. Pick<Type, Keys>
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+// 5. Omit<Type, Keys>
+type MyOmit3<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+// 6. Partial<Type>;
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+// 7. Required<Type>
+type MyRequired<T> = {
+  // -? 很重要
+  // 迭代的时候会把原来的可选标记也带上 所以需要去掉
+  [P in keyof T]-?: T[P];
+};
